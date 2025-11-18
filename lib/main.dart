5a67dd016b3b21ee621ea1527745ae20783ca8c7
shallagehmasood <shallagehmasood@gmail.com>
2025-11-18 09:03:31 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-mport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
@@ -10,9 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const FullScreenWebView(),
+      home: FullScreenWebView(),
     );
   }
 }
@@ -30,16 +31,36 @@ class _FullScreenWebViewState extends State<FullScreenWebView> {
   @override
   void initState() {
     super.initState();
+    // فعال‌سازی حالت فول‌اسکرین
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
+    // تنظیمات WebView
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (url) {
+            debugPrint("Loading: $url");
+          },
+        ),
+      )
       ..loadRequest(Uri.parse("http://178.63.171.244:8000"));
+  }
+
+  Future<bool> _onWillPop() async {
+    if (await _controller.canGoBack()) {
+      _controller.goBack();
+      return false; // مانع خروج از اپ می‌شود
+    }
+    return true; // اجازه خروج از اپ
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: WebViewWidget(controller: _controller),
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: WebViewWidget(controller: _controller),
       ),
     );
   }
