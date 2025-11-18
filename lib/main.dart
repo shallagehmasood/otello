7b9f8b25bebc065d216b0e027435cb734ac1641c
shallagehmasood <shallagehmasood@gmail.com>
2025-11-18 08:51:54 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,45 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'screens/login_screen.dart';
-import 'services/socket_service.dart';
+mport 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => SocketService()),
-      ],
-      child: MaterialApp(
-        title: 'بازی اوتللو آنلاین',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          fontFamily: 'Vazir',
-          useMaterial3: true,
-        ),
-        home: LoginScreen(),
-        debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const FullScreenWebView(),
+    );
+  }
+}
+
+class FullScreenWebView extends StatefulWidget {
+  const FullScreenWebView({super.key});
+
+  @override
+  State<FullScreenWebView> createState() => _FullScreenWebViewState();
+}
+
+class _FullScreenWebViewState extends State<FullScreenWebView> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse("http://178.63.171.244:8000"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(controller: _controller),
       ),
     );
   }
